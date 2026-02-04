d50fd503760c387276af01d073632fd3ca95890e
Kenny Luk <kenny@olgs.com>
2026-02-04 18:59:51 +0800

    Update telemetryRenderer.ts
    mph to kmh

--- src/utils/telemetryRenderer.ts
+++ src/utils/telemetryRenderer.ts
@@ -133,7 +133,7 @@ function drawCenterCluster(
     s: (v: number) => number,
     font: string
 ) {
-    const mph = Math.round(data.vehicleSpeedMps * 0.00);
+    const mph = Math.round(data.vehicleSpeedMps * 3.6);
 
     ctx.save();
     ctx.translate(x, y);
